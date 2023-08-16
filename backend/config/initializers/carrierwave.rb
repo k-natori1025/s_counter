d6694a676960a41490f8e470979eff5568d4f116
knatori1025 <knatori1025@gmail.com>
2023-08-16 12:54:56 +0900

    carrierwaveのhostドメインを本番ドメインのみに設定

--- backend/config/initializers/carrierwave.rb
+++ backend/config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
-  # config.asset_host = "https://backend.s-counter-app.com" || "http://localhost:3001"
-  config.asset_host = ENV['BACKEND_DOMAIN'] || "https://backend.s-counter-app.com"
+  # config.asset_host = ENV['BACKEND_DOMAIN'] 
+  config.asset_host = "https://backend.s-counter-app.com"
   config.storage = :file
   config.cache_storage = :file
 end
